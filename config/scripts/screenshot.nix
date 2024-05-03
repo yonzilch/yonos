e9f011b54ae1074edbc890e6f1165bc9f65b2687
yonzilch <github@yonzilch.com>
2024-05-03 14:58:08 +0800

    nope save screenshot after copy default

--- config/scripts/screenshot.nix
+++ config/scripts/screenshot.nix
@@ -1,5 +1,5 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "screenshootin" ''
-  wayshot -s "$(slurp)" --stdout | satty --filename - --save-after-copy --output-filename ~/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png
+  wayshot -s "$(slurp)" --stdout | satty --filename - --output-filename ~/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png
 ''
