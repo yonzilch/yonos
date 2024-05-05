bb7a6c07e6f663a769d1e22b14fac4573f56223e
yonzilch <git@yonzilch.com>
2024-05-05 14:57:05 +0800

    just up

--- config/home/files.nix
+++ config/home/files.nix
@@ -4,17 +4,19 @@
   # Place Files Inside Home Directory
   home.file.".base16-themes".source = ./files/base16-themes;
   home.file.".config/ascii-fastfetch".source = ./files/ascii-fastfetch;
-  home.file.".config/nushell/config.nu".source = ./files/config.nu;
-  home.file.".config/nushell/env.nu".source = ./files/env.nu;
   home.file.".config/starship.toml".source = ./files/starship.toml;
   home.file.".config/swaylock-bg.jpg".source = ./files/media/swaylock-bg.jpg;
   home.file.".emoji".source = ./files/emoji;
   home.file.".face".source = ./files/face.jpg; # For GDM
   home.file.".face.icon".source = ./files/face.jpg; # For SDDM
-  home.file.".local/share/atuin/init.nu".source = ./files/init.nu;
+  home.file.".local/share/atuin/init.nu".source = ./files/atuin/init.nu;
 
   home.file.".config/anyrun" = {
     source = ./files/anyrun;
+    recursive = true;
+  };
+  home.file.".config/nushell" = {
+    source = ./files/nushell;
     recursive = true;
   };
   home.file.".config/obs-studio" = {
