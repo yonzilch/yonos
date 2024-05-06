80467f0a75b03b863c8f166b07a348461c46202b
yonzilch <git@yonzilch.com>
2024-05-06 12:37:31 +0800

    make fcitx works in vscodium

--- config/home/editor-vscodium.nix
+++ config/home/editor-vscodium.nix
@@ -17,7 +17,7 @@ lib.mkIf (editor-vscodium == true) {
               "--ozone-platform=wayland"
               # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
               # (only supported by chromium/chrome at this time, not electron)
-              "--gtk-version=4"
+              #"--gtk-version=4"
               # make it use text-input-v1, which works for kwin 5.27 and weston
               "--enable-wayland-ime"
 
