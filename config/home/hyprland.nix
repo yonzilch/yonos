a2ebbf6b33ad46c7d11e9386ca27b016b9291d49
yonzilch <github@yonzilch.com>
2024-07-06 13:36:04 +0800

    fix bind of playerctl

--- config/home/hyprland.nix
+++ config/home/hyprland.nix
@@ -218,11 +218,11 @@ in with lib; {
       bind = ALT,Tab,bringactivetotop
       bind = ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
       bind = ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-      binde = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bind = ,XF86AudioPlay, exec, playerctl play-pause
-      bind = ,XF86AudioPause, exec, playerctl play-pause
-      bind = ,XF86AudioNext, exec, playerctl next
-      bind = ,XF86AudioPrev, exec, playerctl previous
+      binde = ,XF86AudioMute,exec,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bind = ,XF86AudioPlay,exec,playerctl play-pause
+      bind = CONTROLALT,Return,exec,playerctl play-pause
+      bind = CONTROLALT,right,exec,playerctl next
+      bind = CONTROLALT,left,exec,playerctl previous
       bind = ,XF86MonBrightnessDown,exec,brightnessctl set 5%-
       bind = ,XF86MonBrightnessUp,exec,brightnessctl set +5%
     '' ];
