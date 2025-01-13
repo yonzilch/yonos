d54ac5894f10e1c917ad0b7863f29601a9939bdb
yonzilch <git@yonzilch.com>
2025-01-13 18:24:23 +0800

    use stubby as DoT daemon

--- Justfile
+++ Justfile
@@ -27,7 +27,7 @@ gc:
 
 generate-hardware-config:
   # generate hardware.nix
-  sudo nixos-generate-config --show-hardware-config > ./hosts/$hostName/hardware.nix
+  sudo nixos-generate-config --show-hardware-config > ./hosts/{{hostname}}/hardware.nix
 
 
 install:
--- modules/tweaks/network-and-ntp.nix
+++ modules/tweaks/network-and-ntp.nix
@@ -1,10 +1,12 @@
 { config, hostname, lib, pkgs, ... }:
 {
   networking = {
+    dhcpcd.extraConfig = "nohook resolv.conf";
     firewall.enable = false;
     hostName = hostname;
+    nameservers = [ "127.0.0.1" "::1" ];
       networkmanager = {
-        dns = "systemd-resolved";
+        dns = "none";
         enable = true;
       };
     timeServers = [
@@ -12,12 +14,47 @@
       "nts.time.nl"
     ];
   };
-  services.resolved = {
-    dnsovertls = "true";
-    domains = [
-      "dot.sb"
-      "dns10.quad9.net"
-    ];
+
+  services.stubby = {
     enable = true;
+    settings = pkgs.stubby.passthru.settingsExample // {
+      resolution_type = "GETDNS_RESOLUTION_STUB";
+      dns_transport_list = ["GETDNS_TRANSPORT_TLS"];
+      tls_authentication = "GETDNS_AUTHENTICATION_REQUIRED";
+      tls_query_padding_blocksize = 256;
+      edns_client_subnet_private = 1;
+      idle_timeout = 10000;
+      listen_addresses = ["127.0.0.1" "0::1"];
+      round_robin_upstreams = 1;
+      upstream_recursive_servers = [{
+        address_data = "185.222.222.222";
+        tls_auth_name = "dot.sb";
+        tls_pubkey_pinset = [{
+          digest = "sha256";
+          value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
+        }];
+      } {
+        address_data = "45.11.45.11";
+        tls_auth_name = "dot.sb";
+        tls_pubkey_pinset = [{
+          digest = "sha256";
+          value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
+        }];
+      } {
+        address_data = "2a09::";
+        tls_auth_name = "dot.sb";
+        tls_pubkey_pinset = [{
+          digest = "sha256";
+          value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
+        }];
+      } {
+        address_data = "2a11::";
+        tls_auth_name = "dot.sb";
+        tls_pubkey_pinset = [{
+          digest = "sha256";
+          value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
+        }];
+      }];
+    };
   };
 }
