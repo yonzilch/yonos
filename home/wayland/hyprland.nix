8a2c53346d9e8d5f8916d25bbea822b1b5cb6e7d
yonzilch <github@yonzilch.com>
2025-01-16 14:33:06 +0800

    first try to fix hy3

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -3,6 +3,9 @@ with lib;
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    plugins = [
+      pkgs.hyprlandPlugins.hy3
+    ];
     systemd.enable = false;
     xwayland.enable = true;
   };
