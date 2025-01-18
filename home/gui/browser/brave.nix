b256dae1c4390a17ca329314fd95ece68bcd9ab3
yonzilch <github@yonzilch.com>
2025-01-18 15:24:25 +0800

    # hashedPassword in example host

--- home/gui/browser/brave.nix
+++ home/gui/browser/brave.nix
@@ -1,6 +1,18 @@
 { pkgs, ...}:
 {
-  home.packages = with pkgs; [
-    brave
-  ];
+  programs.chromium = {
+    commandLineArgs = [
+      "--ozone-platform-hint=auto"
+      "--ozone-platform=wayland"
+      "--wayland-text-input-v3=true"
+    ];
+    enable = true;
+    extensions = [
+      {
+        id = "dcpihecpambacapedldabdbpakmachpb";
+        updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
+      }
+    ];
+    package = pkgs.brave;
+  };
 }
