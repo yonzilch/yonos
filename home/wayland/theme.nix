702596e39768da2c959da243159356af89460425
yonzilch <git@yonzilch.com>
2025-01-19 22:32:19 +0800

    configure gtk iconTheme

--- home/wayland/theme.nix
+++ home/wayland/theme.nix
@@ -11,15 +11,15 @@
   };
 
   gtk = {
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
   };
 
