ddb2ccaab4402d031e73b408909083c5a61837f4
yonzilch <github@yonzilch.com>
2025-01-20 21:14:02 +0800

    update tuigreet arg and waybar conf

--- modules/tweaks/system.nix
+++ modules/tweaks/system.nix
@@ -55,7 +55,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd Hyprland";
           user = username;
         };
       };
