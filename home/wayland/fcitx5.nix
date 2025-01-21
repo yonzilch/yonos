9c5314a49dae2dd97730dc4a3e1350fce3ef6873
yonzilch <github@yonzilch.com>
2025-01-21 22:04:16 +0800

    update fcitx5 config

--- home/wayland/fcitx5.nix
+++ home/wayland/fcitx5.nix
@@ -4,8 +4,6 @@
     enabled = "fcitx5";
     fcitx5 = {
       addons = with pkgs; [
-        fcitx5-catppuccin
-        fcitx5-material-color
         fcitx5-gtk
         fcitx5-rime
         libsForQt5.fcitx5-qt
