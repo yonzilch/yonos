8e398c1b034e5645dd835060167c37ca52338648
yonzilch <git@yonzilch.com>
2025-01-24 21:05:12 +0800

    update system.nix

--- hosts/samyukti/env.nix
+++ hosts/samyukti/env.nix
@@ -8,7 +8,7 @@
   gitUsername = "yonzilch";
   gitEmail = "github@yonzilch.com";
 
-  MonitorSettings = "monitor = , highres, auto, 2";
+  MonitorSettings = "monitor = , highres, auto, 1.6";
   ScaleLevel = "2";
   TimeZone = "Asia/Singapore";
 }
