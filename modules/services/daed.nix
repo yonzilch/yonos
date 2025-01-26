06ba618598eabecdb1c59e3e4f83d76d365f6b89
yonzilch <github@yonzilch.com>
2025-01-26 12:41:39 +0800

    add Transparent-Proxy supported by daed

---
+++ modules/services/daed.nix
@@ -0,0 +1,12 @@
+{ config, hostname, lib, ... }:
+with lib;
+let
+  inherit (import ../../hosts/${hostname}/env.nix) Transparent-Proxy;
+in
+{
+  config = mkIf Transparent-Proxy {
+    services.daed = {
+      enable = true;
+    };
+  };
+}
