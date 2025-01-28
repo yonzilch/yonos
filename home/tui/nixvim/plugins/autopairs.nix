7413c3b858ad9ff9f60f7b246998a3407c69c05d
yonzilch <git@yonzilch.com>
2025-01-28 18:12:52 +0800

    perform deadnix

--- home/tui/nixvim/plugins/autopairs.nix
+++ home/tui/nixvim/plugins/autopairs.nix
@@ -1,4 +1,4 @@
-{
+_: {
   programs.nixvim = {
     plugins.nvim-autopairs = {
       enable = true;
--- home/tui/nixvim/plugins/treesitter.nix
+++ home/tui/nixvim/plugins/treesitter.nix
@@ -1,4 +1,4 @@
-{
+_: {
   programs.nixvim = {
     plugins.treesitter = {
       enable = true;
