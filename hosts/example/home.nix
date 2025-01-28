884b83713aa59600c77b3103e7952ad9aa437155
yonzilch <github@yonzilch.com>
2025-01-28 17:41:15 +0800

    fix example home.nix

--- hosts/example/home.nix
+++ hosts/example/home.nix
@@ -1,9 +1,6 @@
 { lib, username, ... }:
 {
-  imports = [
-    nixvim.homeManagerModules.nixvim
-    ]
-      ++ lib.filesystem.listFilesRecursive ../../home;
+  imports = lib.filesystem.listFilesRecursive ../../home;
 
   home = {
     file = {
