1a34b40bb42bf03bfd5edbfe862742d77f544bba
yonzilch <git@yonzilch.com>
2025-02-13 20:53:17 +0800

    make Keyboard layout and Locale optional

--- hosts/samyukti/env.nix
+++ hosts/samyukti/env.nix
@@ -5,10 +5,11 @@
   GPU-Nvidia = false;
   GPU-Intel = false;
 
+  Locale = "en_US.UTF-8"; # see https://docs.moodle.org/405/en/Table_of_locales
+  TimeZone = "Asia/Singapore"; # see https://en.wikipedia.org/w/index.php?title=List_of_tz_database_time_zones
+
   MonitorSettings = "monitor = , highres, auto, 2";
   ScaleLevel = "2";
-
-  TimeZone = "Asia/Singapore";
 
   Transparent-Proxy = false;
   QEMU-VM-Use-Case = true;
