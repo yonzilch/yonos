ad54b2c63cb47e9d1e174b5434ad506857fd12ec
yonzilch <github@yonzilch.com>
2025-02-13 20:55:18 +0800

    update env.nix

--- hosts/example/env.nix
+++ hosts/example/env.nix
@@ -5,9 +5,9 @@
   GPU-Nvidia = false;
   GPU-Intel = false;
 
-  KeyboardLayout = "us";
+  KeyboardLayout = "us"; # see https://en.wikipedia.org/wiki/Keyboard_layout
   Locale = "en_US.UTF-8"; # see https://docs.moodle.org/405/en/Table_of_locales
-  TimeZone = "America/Chicago"; # see https://en.wikipedia.org/w/index.php?title=List_of_tz_database_time_zones
+  TimeZone = "America/Chicago"; # see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
   MonitorSettings = "monitor = , preferred, auto, 1";
   ScaleLevel = "1";
