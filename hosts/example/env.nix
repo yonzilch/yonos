ca971168b2da9a560b6a713451f7eda8ca8edb05
yonzilch <git@yonzilch.com>
2025-02-13 21:01:37 +0800

    update env.nix in example host

--- hosts/example/env.nix
+++ hosts/example/env.nix
@@ -1,17 +1,17 @@
 {
-  Bluetooth = false;
+  Bluetooth = false; # see https://github.com/blueman-project/blueman
 
-  GPU-AMD = false;
-  GPU-Nvidia = false;
-  GPU-Intel = false;
+  GPU-AMD = false; # see https://nixos.wiki/wiki/AMD_GPU
+  GPU-Nvidia = false; # see https://nixos.wiki/wiki/Nvidia
+  GPU-Intel = false; # see https://nixos.wiki/wiki/Intel_Graphics
 
   KeyboardLayout = "us"; # see https://en.wikipedia.org/wiki/Keyboard_layout
   Locale = "en_US.UTF-8"; # see https://docs.moodle.org/405/en/Table_of_locales
   TimeZone = "America/Chicago"; # see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-  MonitorSettings = "monitor = , preferred, auto, 1";
-  ScaleLevel = "1";
+  MonitorSettings = "monitor = , preferred, auto, 1"; # see https://wiki.hyprland.org/Configuring/Monitors/
+  ScaleLevel = "1"; # see https://wiki.hyprland.org/Configuring/XWayland/#hidpi-xwayland
 
-  Transparent-Proxy = false;
-  QEMU-VM-Use-Case = false;
+  Transparent-Proxy = false; # see https://github.com/daeuniverse/daed
+  QEMU-VM-Use-Case = false; # see https://github.com/qemu/qemu
 }
