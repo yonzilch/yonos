af5725fff35cbd693eb37100067b40ec99ba155e
yonzilch <git@yonzilch.com>
2025-02-13 20:55:18 +0800

    update env.nix

--- hosts/samyukti/env.nix
+++ hosts/samyukti/env.nix
@@ -6,8 +6,8 @@
   GPU-Intel = false;
 
   KeyboardLayout = "us";
-  Locale = "en_US.UTF-8"; # see https://docs.moodle.org/405/en/Table_of_locales
-  TimeZone = "Asia/Singapore"; # see https://en.wikipedia.org/w/index.php?title=List_of_tz_database_time_zones
+  Locale = "en_US.UTF-8";
+  TimeZone = "Asia/Singapore";
 
   MonitorSettings = "monitor = , highres, auto, 2";
   ScaleLevel = "2";
