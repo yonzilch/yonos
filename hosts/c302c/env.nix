7190df2e082662fa657631036291ccd0988db1f7
yonzilch <git@yonzilch.com>
2025-02-15 21:39:30 +0800

    Testing: add host c302c

---
+++ hosts/c302c/env.nix
@@ -0,0 +1,17 @@
+{
+  Bluetooth = false;
+
+  GPU-AMD = false;
+  GPU-Nvidia = false;
+  GPU-Intel = false;
+
+  KeyboardLayout = "us";
+  Locale = "en_US.UTF-8";
+  TimeZone = "Asia/Singapore";
+
+  MonitorSettings = "monitor = , preferred, auto, 1";
+  ScaleLevel = "1";
+
+  Transparent-Proxy = false;
+  QEMU-VM-Use-Case = false;
+}
