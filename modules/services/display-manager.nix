1dd00511e2bc666353eaca136de6a087d860aecd
yonzilch <github@yonzilch.com>
2025-02-20 20:23:20 +0800

    style: make modules clear

---
+++ modules/services/display-manager.nix
@@ -0,0 +1,18 @@
+{ hostname, pkgs, ... }:
+let
+  inherit (import ../../hosts/${hostname}/env.nix) WM;
+in
+{
+  services = {
+    greetd = {
+      enable = true;
+      vt = 1;
+      settings = {
+        default_session = {
+          user = "greeter";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet -c ${WM} -t --user-menu";
+        };
+      };
+    };
+  };
+}
