b506b97a3ea325430df0d136baa6bd6738292750
yonzilch <github@yonzilch.com>
2025-02-21 13:27:34 +0800

    test: try to fix xdg-portal in niri

--- home/wayland/xdg.nix
+++ home/wayland/xdg.nix
@@ -48,6 +48,7 @@
       enable = true;
       extraPortals = [
         pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gnome
         pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal-wlr
       ];
