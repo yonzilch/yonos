963d8d8b7c37f696fdd835fcd3a22dd0dfb1fa12
yonzilch <git@yonzilch.com>
2025-03-09 16:40:14 +0800

    feat: use keyd to define keyboard options

--- hosts/metaphyuni/env.nix
+++ hosts/metaphyuni/env.nix
@@ -9,12 +9,11 @@
 
   KernelPackages = "linuxPackages_cachyos";
   KeyboardLayout = "us";
-  KeyboardOptions = "caps:escape";
   Locale = "en_US.UTF-8";
   TimeZone = "Asia/Singapore";
 
-  MonitorSettings = "monitor = , 2560x1600@165, auto, 1.5";
-  ScaleLevel = "1.5";
+  MonitorSettings = "monitor = , 2560x1600@165, auto, 1.6";
+  ScaleLevel = "1.6";
 
   Transparent-Proxy = false;
   QEMU-VM-Use-Case = true;
