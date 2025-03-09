ac86640e168f1619235a9c89fede36f8dad87d48
yonzilch <git@yonzilch.com>
2025-03-10 00:28:55 +0800

    feat: add Power-Implement and improve style of env.nix

--- hosts/example/env.nix
+++ hosts/example/env.nix
@@ -1,23 +1,47 @@
 {
-  Bluetooth = false; # see https://github.com/blueman-project/blueman
+  # Specific Use Cases
+  ## Hardware related
 
-  BootLoader = "systemd-boot"; # Possible options: grub, grub-mirror, systemd-boot # see https://nixos.wiki/wiki/Bootloader https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=mirroredBoots
+  ### See https://github.com/blueman-project/blueman
+  Bluetooth = true;
+  ### See https://nixos.wiki/wiki/AMD_GPU
+  GPU-AMD = false;
+  ### See https://nixos.wiki/wiki/Nvidia
+  GPU-Nvidia = false;
+  ### See https://nixos.wiki/wiki/Intel_Graphics
+  GPU-Intel = false;
+  ### See https://nixos.wiki/wiki/Power_Management https://nixos.wiki/wiki/Laptop
+  Power-Implement = true;
+  ### See https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/index.html
+  ZFS-Use-Case = true;
 
-  GPU-AMD = false; # see https://nixos.wiki/wiki/AMD_GPU
-  GPU-Nvidia = false; # see https://nixos.wiki/wiki/Nvidia
-  GPU-Intel = false; # see https://nixos.wiki/wiki/Intel_Graphics
+  ## Software related
+  ### See https://github.com/qemu/qemu
+  QEMU-VM-Use-Case = true;
+  ### See https://github.com/daeuniverse/daed
+  Transparent-Proxy = false;
 
-  KernelPackages = "linuxPackages_zen"; # see https://search.nixos.org/options?show=boot.kernelPackages https://www.nyx.chaotic.cx/#using-sched-ext-schedulers
-  KeyboardLayout = "us"; # see https://en.wikipedia.org/wiki/Keyboard_layout
-  Locale = "en_US.UTF-8"; # see https://docs.moodle.org/405/en/Table_of_locales
-  TimeZone = "Asia/Singapore"; # see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-  MonitorSettings = "monitor = , preferred, auto, 1"; # see https://wiki.hyprland.org/Configuring/Monitors/
-  ScaleLevel = "1"; # see https://wiki.hyprland.org/Configuring/XWayland/#hidpi-xwayland
+  # Options
+  ## Basic
 
-  Transparent-Proxy = false; # see https://github.com/daeuniverse/daed
-  QEMU-VM-Use-Case = false; # see https://github.com/qemu/qemu
+  ### Possible options: grub, grub-mirror, systemd-boot # See https://nixos.wiki/wiki/Bootloader https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=mirroredBoots
+  BootLoader = "grub-mirror";
 
-  WM = "Hyprland"; # Possible options: Hyprland, niri
-  ZFS-Use-Case = false; # see https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/index.html
+  ### See https://search.nixos.org/options?show=boot.kernelPackages https://www.nyx.chaotic.cx/#using-sched-ext-schedulers
+  KernelPackages = "linuxPackages_cachyos";
+  ### See https://en.wikipedia.org/wiki/Keyboard_layout
+  KeyboardLayout = "us";
+  ### See https://docs.moodle.org/405/en/Table_of_locales
+  Locale = "en_US.UTF-8";
+  ### See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  TimeZone = "Asia/Singapore";
+
+  ## Wayland related
+  ### See https://wiki.hyprland.org/Configuring/Monitors/
+  MonitorSettings = "monitor = , 2560x1600@165, auto, 1.6";
+  ### See https://wiki.hyprland.org/Configuring/XWayland/#hidpi-xwayland
+  ScaleLevel = "1.6";
+  ### Possible options: Hyprland, niri
+  WM = "Hyprland";
 }
--- hosts/metaphyuni/env.nix
+++ hosts/metaphyuni/env.nix
@@ -1,23 +1,28 @@
 {
+  # Specific Use Cases
+  ## Hardware related
   Bluetooth = true;
-
-  BootLoader = "grub-mirror";
-
   GPU-AMD = false;
   GPU-Nvidia = false;
   GPU-Intel = false;
+  Power-Implement = true;
+  ZFS-Use-Case = true;
 
+  ## Software related
+  QEMU-VM-Use-Case = true;
+  Transparent-Proxy = false;
+
+
+  # Options
+  ## Basic
+  BootLoader = "grub-mirror";
   KernelPackages = "linuxPackages_cachyos";
   KeyboardLayout = "us";
   Locale = "en_US.UTF-8";
   TimeZone = "Asia/Singapore";
 
+  ## Wayland related
   MonitorSettings = "monitor = , 2560x1600@165, auto, 1.6";
   ScaleLevel = "1.6";
-
-  Transparent-Proxy = false;
-  QEMU-VM-Use-Case = true;
-
   WM = "Hyprland";
-  ZFS-Use-Case = true;
 }
