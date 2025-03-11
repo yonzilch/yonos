731e4829251b62eef9279e69c77a4bcd04a71d76
yonzilch <github@yonzilch.com>
2025-03-11 22:37:29 +0800

    test: just update

--- hosts/samyukti/env.nix
+++ hosts/samyukti/env.nix
@@ -1,24 +1,28 @@
 {
-  Bluetooth = true;
-
-  BootLoader = "systemd-boot";
-
+  # Specific Use Cases
+  ## Hardware related
+  Bluetooth = false;
   GPU-AMD = true;
   GPU-Nvidia = false;
-  GPU-Intel = true;
+  GPU-Intel = false;
+  Power-Implement = true;
+  ZFS-Use-Case = true;
 
+  ## Software related
+  QEMU-VM-Use-Case = true;
+  Transparent-Proxy = false;
+
+
+  # Options
+  ## Basic
+  BootLoader = "systemd-boot";
   KernelPackages = "linuxPackages_cachyos";
   KeyboardLayout = "us";
   Locale = "en_US.UTF-8";
   TimeZone = "Asia/Singapore";
 
+  ## Wayland related
   MonitorSettings = "monitor = , highres, auto, 2";
   ScaleLevel = "2";
-
-  Power-Implement = true;
-  Transparent-Proxy = false;
-  QEMU-VM-Use-Case = true;
-
   WM = "Hyprland";
-  ZFS-Use-Case = true;
 }
