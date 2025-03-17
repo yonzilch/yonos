c54809fb37e7915a125d68adb3d32f9e5bb08c39
yonzilch <github@yonzilch.com>
2025-03-17 11:01:13 +0800

    fix: correct indent in neovim

--- home/tui/nixvim/opts.nix
+++ home/tui/nixvim/opts.nix
@@ -2,33 +2,44 @@
 {
   programs.nixvim = {
     opts = {
-    number = true;
-    mouse = "a";
-    showmode = false;
-    clipboard = {
-      providers = {
-        wl-copy = {
-          enable = true;
-          package = pkgs.wl-clipboard-rs;
+      breakindent = true;
+      clipboard = {
+        providers = {
+          wl-copy = {
+            enable = true;
+            package = pkgs.wl-clipboard-rs;
+          };
         };
+        register = "unnamedplus";
       };
-      register = "unnamedplus";
-    };
-    breakindent = true;
-    undofile = true;
-    ignorecase = true;
-    smartcase = true;
-    signcolumn = "yes";
-    updatetime = 250;
-    timeoutlen = 300;
-    splitright = true;
-    splitbelow = true;
-    list = true;
-    listchars.__raw = "{ tab = '» ', trail = '·', nbsp = '␣' }";
-    inccommand = "split";
-    cursorline = true;
-    scrolloff = 10;
-    hlsearch = true;
+      cmdheight = 2;
+      cursorline = true;
+      encoding = "utf-8";
+      expandtab = true;
+      fileencoding = "utf-8";
+      grepprg = "rg --vimgrep";
+      grepformat = "%f:%l:%c:%m";
+      hlsearch = true;
+      ignorecase = true;
+      inccommand = "split";
+      list = true;
+      listchars.__raw = "{ tab = '» ', trail = '·', nbsp = '␣' }";
+      mouse = "a";
+      number = true;
+      pumheight = 0;
+      scrolloff = 8;
+      shiftwidth = 2;
+      showmode = false;
+      signcolumn = "yes";
+      smartcase = true;
+      softtabstop = 2;
+      splitbelow = true;
+      splitright = true;
+      tabstop = 2;
+      termguicolors = true;
+      timeoutlen = 250;
+      undofile = true;
+      updatetime = 50;
     };
   };
 }
