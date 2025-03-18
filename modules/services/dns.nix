66ade6e9e3f4b5f97aeeb249ef8cdd9a20c04db6
yonzilch <github@yonzilch.com>
2025-03-18 18:50:35 +0800

    feat: add quad9 DNS in stubby

--- modules/services/dns.nix
+++ modules/services/dns.nix
@@ -12,35 +12,40 @@
         idle_timeout = 10000;
         listen_addresses = ["127.0.0.1" "0::1"];
         round_robin_upstreams = 1;
-        upstream_recursive_servers = [{
-          address_data = "185.222.222.222";
-          tls_auth_name = "dot.sb";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
-          }];
-        } {
-          address_data = "45.11.45.11";
-          tls_auth_name = "dot.sb";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
-          }];
-        } {
-          address_data = "2a09::";
-          tls_auth_name = "dot.sb";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
-          }];
-        } {
-          address_data = "2a11::";
-          tls_auth_name = "dot.sb";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "amEjS6OJ74LvhMNJBxN3HXxOMSWAriaFoyMQn/Nb5FU=";
-          }];
-        }];
+        upstream_recursive_servers = [
+          {
+            address_data = "185.222.222.222";
+            tls_auth_name = "dot.sb";
+          }
+          {
+            address_data = "45.11.45.11";
+            tls_auth_name = "dot.sb";
+          }
+          {
+            address_data = "2a09::";
+            tls_auth_name = "dot.sb";
+          }
+          {
+            address_data = "2a11::";
+            tls_auth_name = "dot.sb";
+          }
+          {
+            address_data = "9.9.9.9";
+            tls_auth_name = "dns.quad9.net";
+          }
+          {
+            address_data = "149.112.112.112";
+            tls_auth_name = "dns.quad9.net";
+          }
+          {
+            address_data = "2620:fe::fe";
+            tls_auth_name = "dns.quad9.net";
+          }
+          {
+            address_data = "2620:fe::9";
+            tls_auth_name = "dns.quad9.net";
+          }
+        ];
       };
     };
   };
