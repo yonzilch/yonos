f65a95a41a2987aad5e6773c548aa5a4af809394
yonzilch <git@yonzilch.com>
2025-03-21 17:27:54 +0800

    feat: add corestats

---
+++ dotfiles/.config/pragtical/user_settings.lua
@@ -0,0 +1,25 @@
+return {
+  ["config"] = {
+    ["disabled_transitions"] = {
+      ["commandview"] = false,
+      ["contextmenu"] = false,
+      ["logview"] = false,
+      ["nagbar"] = false,
+      ["statusbar"] = false,
+      ["tab_drag"] = false,
+      ["tabs"] = false
+    },
+    ["plugins"] = {
+      ["findfile"] = {
+        ["enable_cache"] = true
+      },
+      ["lineguide"] = {
+        ["enabled"] = true
+      },
+      ["treeview"] = {
+        ["show_hidden"] = true
+      }
+    },
+    ["theme"] = "catppuccin-mocha"
+  }
+}
