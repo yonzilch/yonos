d1f61ff6683195261cff41688891cc0cc0ec16f3
yonzilch <github@yonzilch.com>
2025-03-22 12:43:15 +0800

    test: rm some pkgs

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -7,11 +7,11 @@ in
     services.tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
         CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
