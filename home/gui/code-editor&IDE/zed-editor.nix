adfbbcc6d85be7735657a8d015df55ddb1691879
yonzilch <git@yonzilch.com>
2025-03-27 22:21:27 +0800

    fix: zed-editor userSettings

--- home/gui/code-editor&IDE/zed-editor.nix
+++ home/gui/code-editor&IDE/zed-editor.nix
@@ -8,26 +8,42 @@
     enable = true;
 
     userSettings = {
-      auto_install_extensions = {
-        catpuccin = true;
-        catpucchin-icons = true;
-        nix = true;
-      };
-      features = {
-        copilot = false;
-        inline_completion_provider = "none";
-      };
       assistant = {
         enabled = false;
         version = "2";
       };
-      hour_format = "hour24";
+      auto_install_extensions = {
+        catppuccin = true;
+        catppucchin-icons = true;
+        nix = true;
+      };
       auto_update = false;
+      base_keymap = "VSCode";
+      features = {
+        copilot = false;
+        inline_completion_provider = "none";
+      };
+      hour_format = "hour24";
+      icon_theme = {
+        dark = "Catppuccin Mocha";
+        light = "Catppuccin Mocha";
+        mode = "system";
+      };
+      load_direnv = "shell_hook";
+      lsp = {
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+      };
+      package = pkgs.zed-editor-fhs;
+      show_whitespaces = "all";
       terminal = {
         alternate_scroll = "off";
         blinking = "off";
+        button = false;
         copy_on_select = false;
-        dock = "bottom";
         detect_venv = {
           on = {
             directories = [
@@ -39,47 +55,27 @@
             activate_script = "default";
           };
         };
-        toolbar = {
-          breadcrumbs = false;
-        };
-        env = {
-          TERM = "alacritty";
-        };
+        dock = "bottom";
         font_family = "JetBrainsMono Nerd Font";
         font_features = null;
         font_size = null;
         line_height = "comfortable";
-        option_as_meta = false;
-        button = false;
         shell = "system";
+        toolbar = {
+          breadcrumbs = false;
+        };
+        option_as_meta = false;
         toolbar = {
           title = true;
         };
         working_directory = "current_project_directory";
-      };
-      lsp = {
-        nix = {
-          binary = {
-            path_lookup = true;
-          };
-        };
       };
       theme = {
         dark = "Catppuccin Mocha";
         light = "Catppuccin Mocha";
         mode =  "system";
       };
-      icon_theme = {
-        dark = "Catppuccin Mocha";
-        light = "Catppuccin Mocha";
-        mode = "system";
-      };
       vim_mode = false;
-      ## tell zed to use direnv and direnv can use a flake.nix enviroment.
-      load_direnv = "shell_hook";
-      base_keymap = "VSCode";
-      show_whitespaces = "all";
-      package = pkgs.zed-editor-fhs;
     };
   };
 }
