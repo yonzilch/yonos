2efc392e09def2c028269550121c537e670d8579
yonzilch <git@yonzilch.com>
2025-03-31 20:33:46 +0800

    feat: update swayosd and ntp service

---
+++ modules/services/ntp.nix
@@ -0,0 +1,30 @@
+{ lib, ...}:
+with lib;
+{
+  services.ntpd-rs = {
+    enable = true;
+    settings = {
+      source = [
+        {
+          address = "ntppool1.time.nl";
+          mode = "nts";
+        }
+        {
+          address = "ntppool2.time.nl";
+          mode = "nts";
+        }
+        {
+          address = "nts.netnod.se";
+          mode = "nts";
+        }
+      ];
+    };
+    useNetworkingTimeServers = false;
+  };
+  systemd.services.ntpd-rs = {
+    after = mkForce [];
+    requires = mkForce [];
+    wantedBy = mkForce [];
+    wants = mkForce [];
+  };
+}
