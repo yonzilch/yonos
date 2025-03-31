e720bdc8c6730d107b1e91ef586fa6a9e26f0c15
yonzilch <git@yonzilch.com>
2025-03-31 14:51:42 +0800

    feat: declare dconf standalone

---
+++ home/wayland/dconf.nix
@@ -0,0 +1,20 @@
+{ hostname, lib, ... }:
+with lib;
+let
+  inherit (import ../../hosts/${hostname}/env.nix) QEMU-VM-Use-Case;
+in
+{
+  dconf.settings = mkMerge [
+    {
+      "org/nemo/preferences" = {
+        "default-folder-viewer" = "list-view";
+      };
+    }
+    (mkIf QEMU-VM-Use-Case {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    })
+  ];
+}
