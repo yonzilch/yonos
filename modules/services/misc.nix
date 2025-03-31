b46c01efc8dc6fd2930e15b05277f54d9eb4b747
yonzilch <github@yonzilch.com>
2025-03-31 20:33:46 +0800

    feat: update swayosd and ntp service

--- modules/services/misc.nix
+++ modules/services/misc.nix
@@ -6,26 +6,6 @@ _: {
     };
     gvfs.enable = true;
     libinput.enable = true;
-    ntpd-rs = {
-      enable = true;
-      settings = {
-        source = [
-          {
-            address = "ntppool1.time.nl";
-            mode = "nts";
-          }
-          {
-            address = "ntppool2.time.nl";
-            mode = "nts";
-          }
-          {
-            address = "nts.netnod.se";
-            mode = "nts";
-          }
-        ];
-      };
-      useNetworkingTimeServers = false;
-    };
     scx = {
       enable = true;
       scheduler = "scx_lavd";
