bff5b35e46e6ee96ec8b08dda13b0b54cd57e171
yonzilch <github@yonzilch.com>
2025-04-02 21:20:52 +0800

    test: try to use hyprpolkitagent instead of mate-polkit

--- home/wayland/niri.nix
+++ home/wayland/niri.nix
@@ -5,28 +5,6 @@ in
 with lib;
 mkIf (WM == "niri")
 {
-  systemd.user.targets.niri-session = {
-    Unit = {
-      Description = "niri compositor session";
-      Documentation = "man:systemd.special(7)";
-    };
-    Target = {
-      After = "graphical-session-pre.target";
-      BindsTo = "graphical-session.target";
-      Wants = "graphical-session-pre.target";
-      DefaultDependencies = false;
-    };
-  };
-  systemd.user.services.xdg-desktop-portal-gnome = {
-    Unit = {
-      Description = "Portal service (GNOME implementation)";
-    };
-    Service = {
-      Type = "dbus";
-      BusName = "org.freedesktop.impl.portal.desktop.gnome";
-      ExecStart = "${pkgs.xdg-desktop-portal-gnome}/libexec/xdg-desktop-portal-gnome";
-    };
-  };
   home.packages = with pkgs; [
     niri
   ];
@@ -43,5 +21,27 @@ mkIf (WM == "niri")
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-gnome
     ];
+  };
+  systemd.user.targets.niri-session = {
+    Target = {
+      After = "graphical-session-pre.target";
+      BindsTo = "graphical-session.target";
+      DefaultDependencies = false;
+      Wants = "graphical-session-pre.target";
+    };
+    Unit = {
+      Description = "niri compositor session";
+      Documentation = "man:systemd.special(7)";
+    };
+  };
+  systemd.user.services.xdg-desktop-portal-gnome = {
+    Unit = {
+      Description = "Portal service (GNOME implementation)";
+    };
+    Service = {
+      Type = "dbus";
+      BusName = "org.freedesktop.impl.portal.desktop.gnome";
+      ExecStart = "${pkgs.xdg-desktop-portal-gnome}/libexec/xdg-desktop-portal-gnome";
+    };
   };
 }
