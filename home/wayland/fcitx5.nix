7e7f1bb65af1f593e088a93f409dc6a82bb68f24
yonzilch <git@yonzilch.com>
2025-04-07 09:14:17 +0800

    feat: mk rime-ice fetchFromGithub

--- home/wayland/fcitx5.nix
+++ home/wayland/fcitx5.nix
@@ -1,4 +1,15 @@
 {pkgs, ...}: {
+  home.file.".local/share/fcitx5/rime" = {
+    force = true;
+    recursive = true;
+    source = pkgs.fetchFromGitHub {
+      hash = "sha256-s3r8cdEliiPnKWs64Wgi0rC9Ngl1mkIrLnr2tIcyXWw=";
+      owner = "iDvel";
+      repo = "rime-ice";
+      tag = "2025.04.06";
+    };
+  };
+
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5 = {
