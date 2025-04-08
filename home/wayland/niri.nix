c97c3e46492ddfd92d37c6fccb1f42e644e9b5c7
yonzilch <github@yonzilch.com>
2025-04-08 20:27:08 +0800

    test: xdg-desktop-portal-gnome on niri

--- home/wayland/niri.nix
+++ home/wayland/niri.nix
@@ -26,10 +26,10 @@ in
       xdg.portal = {
         config = {
           common = {
-            default = ["gnome" "gtk"];
+            default = ["gtk"];
+            "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
             "org.freedesktop.impl.portal.ScreenCast" = "gnome";
             "org.freedesktop.impl.portal.Screenshot" = "gnome";
-            "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
           };
         };
         extraPortals = [
