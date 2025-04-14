2ccdf61af77cd92a6109788a4432d6da5fe2d83b
yonzilch <github@yonzilch.com>
2025-04-14 22:41:41 +0800

    feat: declare dconf in specific programs

--- home/gui/file-manager/nemo.nix
+++ home/gui/file-manager/nemo.nix
@@ -1,4 +1,13 @@
 {pkgs, ...}: {
+  dconf.settings = {
+    "org/nemo/preferences" = {
+      "date-font-choice" = "no-mono";
+      "date-format" = "iso";
+      "default-folder-viewer" = "list-view";
+      "show-hidden-files" = true;
+    };
+  };
+
   home.packages = with pkgs; [
     nemo
   ];
