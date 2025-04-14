86dab4eb9453742fe5522c2702d6b5df1001f168
yonzilch <git@yonzilch.com>
2025-04-14 22:41:41 +0800

    feat: declare dconf in specific programs

--- home/gui/virtual-machine-manager/virt-manager.nix
+++ home/gui/virtual-machine-manager/virt-manager.nix
@@ -8,6 +8,13 @@
 in
   with lib;
     mkIf QEMU-VM-Use-Case {
+      dconf.settings = {
+        "org/virt-manager/virt-manager/connections" = {
+          autoconnect = ["qemu:///system"];
+          uris = ["qemu:///system"];
+        };
+      };
+
       home.packages = with pkgs; [
         virt-manager
       ];
--- home/wayland/waytrogen.nix
+++ home/wayland/waytrogen.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
+  dconf.settings = {
+    "org/Waytrogen/Waytrogen" = {
+      "wallpaper-folder" = "${config.home.homeDirectory}/Pictures/Wallpapers";
+    };
+  };
+
   home.packages = with pkgs; [
     waytrogen
   ];
