63a62c117ab4b308dd5520a97391ec46b699d5ea
yonzilch <git@yonzilch.com>
2025-05-22 16:54:49 +0800

    feat: update xdg.mimeApps

--- home/wayland/xdg.nix
+++ home/wayland/xdg.nix
@@ -5,26 +5,32 @@
       archiver-extract-smart = ["peazip-extract-smart.desktop"];
       archiver-useless-options = [
         "peazip-extract.desktop"
-        "peazip-extract-here.desktop"
         "peazip-extract-desktop.desktop"
         "peazip-extract-documents.desktop"
         "peazip-extract-downloads.desktop"
+        "peazip-extract-here.desktop"
       ];
       browser = ["brave-browser.desktop"];
-      editor = ["nvim.desktop"];
+      editor = ["dev.zed.Zed.desktop"];
       filemanager = ["nemo.desktop"];
       image-viewer = ["oculante.desktop"];
       media-player = ["mpv.desktop"];
     in {
-      associations.removed = {
-        "application/iso" = archiver-useless-options;
-        "application/zip" = archiver-useless-options;
-        "application/x-7z-compressed" = archiver-useless-options;
-        "application/x-ace-compressed" = archiver-useless-options;
-        "application/x-bzip2" = archiver-useless-options;
-        "application/x-gzip" = archiver-useless-options;
-        "application/x-tar" = archiver-useless-options;
-        "application/x-rar-compressed" = archiver-useless-options;
+      associations = {
+        added = {
+          "application/x-directory" = ["peazip-add-to-archive.desktop"];
+          "inode/directory" = ["peazip-add-to-archive.desktop"];
+        };
+        removed = {
+          "application/iso" = archiver-useless-options;
+          "application/x-7z-compressed" = archiver-useless-options;
+          "application/x-ace-compressed" = archiver-useless-options;
+          "application/x-bzip2" = archiver-useless-options;
+          "application/x-gzip" = archiver-useless-options;
+          "application/x-rar-compressed" = archiver-useless-options;
+          "application/x-tar" = archiver-useless-options;
+          "application/zip" = archiver-useless-options;
+        };
       };
       enable = true;
       defaultApplications = {
@@ -34,16 +40,16 @@
         "application/x-ace-compressed" = archiver-useless-options;
         "application/x-bzip2" = archiver-extract-smart;
         "application/x-gzip" = archiver-extract-smart;
-        "application/x-tar" = archiver-extract-smart;
         "application/x-rar-compressed" = archiver-extract-smart;
+        "application/x-tar" = archiver-extract-smart;
 
         "application/json" = browser;
         "application/pdf" = browser;
         "application/rdf+xml" = browser;
         "application/rss+xml" = browser;
-        "application/xml" = browser;
         "application/xhtml+xml" = browser;
         "application/xhtml_xml" = browser;
+        "application/xml" = browser;
         "application/x-extension-htm" = browser;
         "application/x-extension-html" = browser;
         "application/x-extension-shtml" = browser;
