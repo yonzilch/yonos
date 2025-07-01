8c764d272d287025fedc996d3bb39c6addd185fb
yonzilch <github@yonzilch.com>
2025-07-01 17:01:17 +0800

    test: try to fix NVIDIA Cards crash

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -3,9 +3,9 @@
   lib,
   pkgs,
   ...
-}: let
-  inherit
-    (import ../../hosts/${hostname}/env.nix)
+}:
+let
+  inherit (import ../../hosts/${hostname}/env.nix)
     GPU-Nvidia
     MonitorSettings
     KeyboardLayout
@@ -13,52 +13,48 @@
     WM
     ;
 in
-  with lib;
-    mkIf (WM == "Hyprland")
-    {
-      home.packages = with pkgs; [
-        xorg.xprop
+with lib;
+mkIf (WM == "Hyprland") {
+  home.packages = with pkgs; [
+    xorg.xprop
+  ];
+  wayland.windowManager.hyprland = {
+    enable = true;
+    plugins = [
+      pkgs.hyprlandPlugins.hy3
+    ];
+    systemd = {
+      enable = true;
+      variables = [ "--all" ];
+    };
+    xwayland.enable = true;
+    extraConfig = concatStrings [
+      ''
+        ${MonitorSettings}
+        $KEYBOARDLAYOUT = ${KeyboardLayout}
+        $SCALE = ${ScaleLevel}
+        source = ~/.config/hypr/hyprland/*
+      ''
+    ];
+    settings = mkIf GPU-Nvidia {
+      env = [
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        "GBM_BACKEND,nvidia-drm"
+        "LIBVA_DRIVER_NAME,nvidia"
+        "NVD_BACKEND,direct"
+        "WLR_NO_HARDWARE_CURSORS,1"
       ];
-      wayland.windowManager.hyprland = {
-        enable = true;
-        plugins = [
-          pkgs.hyprlandPlugins.hy3
-        ];
-        systemd = {
-          enable = true;
-          variables = ["--all"];
-        };
-        xwayland.enable = true;
-        extraConfig = concatStrings [
-          ''
-            ${MonitorSettings}
-            $KEYBOARDLAYOUT = ${KeyboardLayout}
-            $SCALE = ${ScaleLevel}
-            source = ~/.config/hypr/hyprland/*
-          ''
-        ];
-        settings = mkIf GPU-Nvidia {
-          env = [
-            # for hyprland with nvidia gpu, ref https://wiki.hyprland.org/Nvidia/
-            "LIBVA_DRIVER_NAME,nvidia"
-            "XDG_SESSION_TYPE,wayland"
-            "GBM_BACKEND,nvidia-drm"
-            "AQ_DRM_DEVICES,/dev/dri/card1"
-            "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-            # fix https://github.com/hyprwm/Hyprland/issues/1520
-            "WLR_NO_HARDWARE_CURSORS,1"
-          ];
-        };
+    };
+  };
+  xdg.portal = {
+    config = {
+      common = {
+        default = [ "gtk" ];
       };
-      xdg.portal = {
-        config = {
-          common = {
-            default = ["gtk"];
-          };
-        };
-        extraPortals = [
-          pkgs.xdg-desktop-portal-gtk
-          pkgs.xdg-desktop-portal-hyprland
-        ];
-      };
-    }
+    };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+  };
+}
