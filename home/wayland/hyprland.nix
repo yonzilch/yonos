63d808e1a6fbf26d163afcea4726cd4c150d7e69
yonzilch <github@yonzilch.com>
2025-08-05 22:37:11 +0800

    style: just format

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -3,9 +3,9 @@
   lib,
   pkgs,
   ...
-}:
-let
-  inherit (import ../../hosts/${hostname}/env.nix)
+}: let
+  inherit
+    (import ../../hosts/${hostname}/env.nix)
     GPU-Nvidia
     MonitorSettings
     KeyboardLayout
@@ -13,48 +13,48 @@ let
     WM
     ;
 in
-with lib;
-mkIf (WM == "Hyprland") {
-  home.packages = with pkgs; [
-    xorg.xprop
-  ];
-  wayland.windowManager.hyprland = {
-    enable = true;
-    plugins = [
-      pkgs.hyprlandPlugins.hy3
-    ];
-    systemd = {
-      enable = true;
-      variables = [ "--all" ];
-    };
-    xwayland.enable = true;
-    extraConfig = concatStrings [
-      ''
-        ${MonitorSettings}
-        $KEYBOARDLAYOUT = ${KeyboardLayout}
-        $SCALE = ${ScaleLevel}
-        source = ~/.config/hypr/hyprland/*
-      ''
-    ];
-    settings = mkIf GPU-Nvidia {
-      env = [
-        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-        "GBM_BACKEND,nvidia-drm"
-        "LIBVA_DRIVER_NAME,nvidia"
-        "NVD_BACKEND,direct"
-        "WLR_NO_HARDWARE_CURSORS,1"
+  with lib;
+    mkIf (WM == "Hyprland") {
+      home.packages = with pkgs; [
+        xorg.xprop
       ];
-    };
-  };
-  xdg.portal = {
-    config = {
-      common = {
-        default = [ "gtk" ];
+      wayland.windowManager.hyprland = {
+        enable = true;
+        plugins = [
+          pkgs.hyprlandPlugins.hy3
+        ];
+        systemd = {
+          enable = true;
+          variables = ["--all"];
+        };
+        xwayland.enable = true;
+        extraConfig = concatStrings [
+          ''
+            ${MonitorSettings}
+            $KEYBOARDLAYOUT = ${KeyboardLayout}
+            $SCALE = ${ScaleLevel}
+            source = ~/.config/hypr/hyprland/*
+          ''
+        ];
+        settings = mkIf GPU-Nvidia {
+          env = [
+            "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+            "GBM_BACKEND,nvidia-drm"
+            "LIBVA_DRIVER_NAME,nvidia"
+            "NVD_BACKEND,direct"
+            "WLR_NO_HARDWARE_CURSORS,1"
+          ];
+        };
       };
-    };
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
-    ];
-  };
-}
+      xdg.portal = {
+        config = {
+          common = {
+            default = ["gtk"];
+          };
+        };
+        extraPortals = [
+          pkgs.xdg-desktop-portal-gtk
+          pkgs.xdg-desktop-portal-hyprland
+        ];
+      };
+    }
