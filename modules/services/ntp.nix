5691347dfb887239e5875419c7a2f3ed0c4e81b4
yonzilch <github@yonzilch.com>
2025-08-11 15:10:28 +0800

    feat: mk TimeSync-Method optional

--- modules/services/ntp.nix
+++ modules/services/ntp.nix
@@ -1,21 +1,43 @@
-_: {
+{
+  hostname,
+  lib,
+  ...
+}:
+with lib; let
+  inherit (import ../../hosts/${hostname}/env.nix) TimeSync-Method;
+in {
   services.ntpd-rs = {
     enable = true;
     settings = {
-      source = [
-        {
-          address = "ntppool1.time.nl";
-          mode = "nts";
-        }
-        {
-          address = "ntppool2.time.nl";
-          mode = "nts";
-        }
-        {
-          address = "nts.netnod.se";
-          mode = "nts";
-        }
-      ];
+      source =
+        (optionals (TimeSync-Method == "NTS") [
+          {
+            address = "ntppool1.time.nl";
+            mode = "nts";
+          }
+          {
+            address = "ntppool2.time.nl";
+            mode = "nts";
+          }
+          {
+            address = "nts.netnod.se";
+            mode = "nts";
+          }
+        ])
+        ++ (optionals (TimeSync-Method == "Plain") [
+          {
+            address = "ntp.time.nl";
+            mode = "server";
+          }
+          {
+            address = "ntp.netnod.se";
+            mode = "server";
+          }
+          {
+            address = "time.cloudflare.com";
+            mode = "server";
+          }
+        ]);
     };
     useNetworkingTimeServers = false;
   };
