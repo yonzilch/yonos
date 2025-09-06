89e3630cbeb89e7f44baa86cc2438fb0f753e95b
yonzilch <github@yonzilch.com>
2025-09-06 19:55:35 +0800

    chore: update env in host metaphyuni

--- hosts/metaphyuni/env.nix
+++ hosts/metaphyuni/env.nix
@@ -2,16 +2,15 @@
   # Specific Use Cases
   ## Hardware related
   Bluetooth = true;
-  GPU-AMD = false;
+  GPU-AMD = true;
   GPU-Nvidia = false;
   GPU-Intel = false;
   HotSpot-Use-Case = true;
-  ZFS-Networking-HostID = "60114255";
   ZFS-Use-Case = true;
 
   ## Software related
   QEMU-VM-Use-Case = true;
-  Transparent-Proxy = false;
+  Transparent-Proxy = true;
 
   # Options
   ## Basic
@@ -20,13 +19,15 @@
   KernelPackages = "linuxPackages_cachyos";
   KeyboardLayout = "us";
   Locale = "en_US.UTF-8";
-  StateVersion = "25.05";
+  StateVersion = "25.11";
   TimeSync-Method = "NTS";
   TimeZone = "Asia/Singapore";
 
   ## Wayland related
-  MonitorSettings = "monitor = , 2560x1600@100, auto, 1.6";
-  OutputSettings = "output * resolution 2560x1600@165Hz scale 1.6";
-  ScaleLevel = "1.6";
+  MonitorSettings = "
+    monitor = eDP-1, 2560x1600@100, auto, 1.6
+    monitor = DP-1, 3840x2160@144, auto, 2
+    ";
+  ScaleLevel = "2";
   WM = "Hyprland";
 }
