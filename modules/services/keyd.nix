20b49994c8a0199f47aacf86ecdf53295d1f9208
yonzilch <github@yonzilch.com>
2025-09-18 19:45:36 +0800

    style: use environment.etc to declare config of keyd

--- modules/services/keyd.nix
+++ modules/services/keyd.nix
@@ -1,25 +1,45 @@
 _: {
+  environment.etc."/keyd/default.conf" = {
+    mode = "0600";
+    text = ''
+      [ids]
+      *
+
+      [alt]
+      c=backspace
+      capslock=capslock
+      d=delete
+      e=enter
+      h=left
+      j=down
+      k=up
+      l=right
+
+      [main]
+      capslock=overload(alt, esc)
+    '';
+  };
   services.keyd = {
     enable = true;
-    keyboards = {
-      default = {
-        ids = ["*"];
-        settings = {
-          main = {
-            capslock = "overload(alt, esc)";
-          };
-          "alt" = {
-            c = "backspace";
-            d = "delete";
-            e = "enter";
-            h = "left";
-            j = "down";
-            k = "up";
-            l = "right";
-            capslock = "capslock";
-          };
-        };
-      };
-    };
+    # keyboards = {
+    #   default = {
+    #     ids = [ "*" ];
+    #     settings = {
+    #       main = {
+    #         capslock = "overload(alt, esc)";
+    #       };
+    #       "alt" = {
+    #         c = "backspace";
+    #         d = "delete";
+    #         e = "enter";
+    #         h = "left";
+    #         j = "down";
+    #         k = "up";
+    #         l = "right";
+    #         capslock = "capslock";
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
