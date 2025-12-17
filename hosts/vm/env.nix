6279e895fff50328bb458f593f4cc2d3ad3f0493
yonzilch <git@yonzilch.com>
2025-12-18 00:51:25 +0800

    feat: rewrite dns & network config and tune env.nix

--- hosts/vm/env.nix
+++ hosts/vm/env.nix
@@ -16,7 +16,7 @@
   # Options
   ## Basic
   BootLoader = "grub";
-  DNS-Method = "DoT";
+  DNS-Method = "NetworkManager";
   KernelPackages = "linuxPackages_cachyos";
   KeyboardLayout = "us";
   Locale = "en_US.UTF-8";
