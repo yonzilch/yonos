d524cac1d4a7e48dc6678c1065442ec048709a67
yonzilch <git@yonzilch.com>
2026-04-28 15:18:47 +0800

    chore: rm chaotic-nyx in nix substituters

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -21,7 +21,10 @@ with lib; {
     settings = {
       auto-optimise-store = true;
       builders-use-substitutes = true;
-      experimental-features = ["nix-command" "flakes"];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       connect-timeout = 5;
       gc-keep-derivations = false;
       gc-keep-outputs = false;
@@ -31,13 +34,11 @@ with lib; {
       substituters = [
         "https://cache.garnix.io" # See https://github.com/daeuniverse/flake.nix
         "https://cache.nixos.org" # See https://nixos.wiki/wiki/Binary_Cache
-        "https://chaotic-nyx.cachix.org" # See https://github.com/chaotic-cx/nyx
         "https://nix-community.cachix.org" # See https://nix-community.org/cache/
       ];
       trusted-public-keys = [
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       warn-dirty = false;
