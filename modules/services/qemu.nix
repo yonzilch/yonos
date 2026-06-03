cd3f94f362a62cf2ca2898a4f6bfc7e0c6c2a735
yonzilch <git@yonzilch.com>
2026-06-03 12:36:51 +0800

    chore: rm spice-webdavd

--- modules/services/qemu.nix
+++ modules/services/qemu.nix
@@ -9,7 +9,6 @@ in {
   config = mkIf QEMU-VM-Use-Case {
     services = {
       spice-vdagentd.enable = true;
-      spice-webdavd.enable = true;
     };
     virtualisation.libvirtd.enable = true;
   };
