ac0f32cabe5b317c91eaf6f949f1bd041cc7f99f
yonzilch <git@yonzilch.com>
2026-06-06 16:44:45 +0800

    style: mv `xprop` command from dotfiles to `sway.nix`

--- home/wayland/sway.nix
+++ home/wayland/sway.nix
@@ -23,7 +23,7 @@ in
           BindsTo = "graphical-session.target";
           Conflicts = "shutdown.target";
           DefaultDependencies = false;
-          Description = "sway compositor session";
+          Description = "${WM} compositor session";
           Documentation = "man:systemd.special(7)";
           Wants = "graphical-session-pre.target";
         };
@@ -32,16 +32,12 @@ in
         configFile."sway/config".text = concatStrings [
           ''
             ${OutputSettings}
-            set $SCALE ${ScaleLevel}
+            exec_always xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE ${ScaleLevel}
             include $HOME/.config/sway/*
           ''
         ];
         portal = {
-          config = {
-            common = {
-              default = ["gtk"];
-            };
-          };
+          config.common.default = ["gtk"];
           extraPortals = [
             pkgs.xdg-desktop-portal-gtk
             pkgs.xdg-desktop-portal-wlr
