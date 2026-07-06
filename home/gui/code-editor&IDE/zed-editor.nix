ad6fd05dc1ea2d9c0739628998f0f8038f9f6300
yonzilch <git@yonzilch.com>
2026-07-06 21:10:49 +0800

    chore: configure zed-editor's Nix formatting with nixfmt
    Use nixfmt as the external formatter for Nix files in Zed and enable
    format-on-save. Pin the formatter and nixd binaries to Nix store paths
    so Zed can find them reliably when launched outside an interactive shell.
    
    Also explicitly use nixd for Nix language support and disable nil to avoid
    conflicting language server behavior.

--- home/gui/code-editor&IDE/zed-editor.nix
+++ home/gui/code-editor&IDE/zed-editor.nix
@@ -18,22 +18,28 @@
     userSettings = {
       auto_update = false;
       base_keymap = "VSCode";
+
       completions = {
         lsp_fetch_timeout_ms = 5000;
       };
+
       helix_mode = false;
+
       journal = {
         hour_format = "hour24";
       };
+
       icon_theme = {
         dark = "Catppuccin Mocha";
         light = "Catppuccin Mocha";
         mode = "system";
       };
+
       languages = {
         Nix = {
           language_servers = [
             "nixd"
+            "!nil"
           ];
 
           formatter = {
@@ -47,23 +53,37 @@
           tab_size = 2;
         };
       };
+
       load_direnv = "shell_hook";
+
       lsp = {
         nixd = {
           binary = {
             path = "${pkgs.nixd}/bin/nixd";
           };
+
+          initialization_options = {
+            formatting = {
+              command = [
+                "${pkgs.nixfmt}/bin/nixfmt"
+              ];
+            };
+          };
         };
       };
+
       project_panel = {
         dock = "left";
       };
+
       show_whitespaces = "all";
+
       terminal = {
         alternate_scroll = "off";
         blinking = "off";
         button = false;
         copy_on_select = false;
+
         detect_venv = {
           on = {
             directories = [
@@ -75,23 +95,28 @@
             activate_script = "default";
           };
         };
+
         dock = "bottom";
         font_family = "JetBrainsMono Nerd Font Mono";
         font_features = null;
         font_size = 16;
         line_height = "comfortable";
         shell = "system";
+
         toolbar = {
           breadcrumbs = false;
         };
+
         option_as_meta = false;
         working_directory = "current_project_directory";
       };
+
       theme = {
         dark = "Tokyo Night";
         light = "Catppuccin Mocha";
         mode = "system";
       };
+
       ui_font_family = "Sarasa Gothic SC";
       ui_font_size = 16.5;
       vim_mode = false;
