2dff4d904d6559d9f76f78e6c5876488f3ec14ae
yonzilch <git@yonzilch.com>
2026-07-17 21:45:41 +0800

    feat!: remove garnix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -33,12 +33,10 @@ with lib;
       log-lines = 25;
       nix-path = mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
       substituters = [
-        "https://cache.garnix.io" # See https://github.com/daeuniverse/flake.nix
         "https://cache.nixos.org" # See https://nixos.wiki/wiki/Binary_Cache
         "https://nix-community.cachix.org" # See https://nix-community.org/cache/
       ];
       trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
